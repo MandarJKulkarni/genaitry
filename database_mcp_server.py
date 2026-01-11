ab87f991963db9cfe0cebbbcbfb338c8ef2de84e
MandarJKulkarni <33712629+MandarJKulkarni@users.noreply.github.com>
2026-01-11 22:24:01 +0530

    Update database_mcp_server.py to use oauth server

--- database_mcp_server.py
+++ database_mcp_server.py
@@ -1,6 +1,20 @@
 from mcp.server.fastmcp import FastMCP
 import json
-mcp = FastMCP("MYDATABASE")
+from fastmcp.server.auth.providers.jwt import JWTVerifier
+from fastmcp.server.auth import RemoteAuthProvider
+
+jwtverifier = JWTVerifier(
+    jwks_uri="your_jwks_uri",
+    issuer="your_jwt_issuer",
+    audience=["your_audience space_separated"]   # Custom audience for MCP server
+)
+
+auth = RemoteAuthProvider(
+    token_verifier=jwtverifier,
+    authorization_servers=[AnyHttpUrl("authorization_server_url")],
+    base_url="http://localhost:8000"
+)
+mcp = FastMCP(name="MYDATABASE", auth=auth)
 
 @mcp.tool()
 def search(search_term: str, collection_name: str, authorization: str) -> str:
@@ -13,4 +27,4 @@ def store(record: json, collection_name: str, authorization: str) -> str:
     return "Record ingested successfully"
 
 if __name__ == "__main__":
-    mcp.run(transport="stdio")
+    mcp.run(transport="streamable-http", host="localhost", port=8000,log_level="DEBUG")
